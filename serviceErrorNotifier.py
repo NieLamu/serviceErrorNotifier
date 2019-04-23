7e3450a2ce1b7e121e1aca7dc8498541747d25b8
NieLamu <mxmxlty@gmail.com>
2019-04-23 13:50:44 +0800

    refactor: remove config

--- serviceErrorNotifier.py
+++ serviceErrorNotifier.py
@@ -16,30 +16,23 @@ from email.header import Header
 from smtplib import SMTP_SSL
 
 
-logger = logging.getLogger(__name__)
-logger.setLevel(level=logging.INFO)
-# StreamHandler
-stream_handler = logging.StreamHandler(sys.stdout)
-stream_handler.setLevel(level=logging.DEBUG)
-logger.addHandler(stream_handler)
-
-
-sendEmailConfig = os.path.join(os.path.dirname(__file__), 'sendEmailConfig.json')
+sendEmailConfig = os.path.join(
+    os.path.dirname(__file__), 'sendEmailConfig.json')
 
 
 def sendEmail(mail_content='测试', mail_title='辣鸡，又出错了...'):
     with open(sendEmailConfig, 'r') as f:
         config = json.load(f)
-    #ssl登录
+    # ssl登录
     smtp = SMTP_SSL(config["server"])
-    #set_debuglevel()是用来调试的。参数值为1表示开启调试模式，参数值为0关闭调试模式
+    # set_debuglevel()是用来调试的。参数值为1表示开启调试模式，参数值为0关闭调试模式
     smtp.set_debuglevel(0)
     smtp.ehlo(config["server"])
     smtp.login(config["sender"], config["pwd"])
 
     msg = MIMEText(str(mail_content), "plain", 'utf-8')
     msg["Subject"] = Header(mail_title, 'utf-8')
-    msg["From"] = config["senderName"]+"<%s>"%config["sender"]
+    msg["From"] = config["senderName"]+"<%s>" % config["sender"]
     for receiver in config["receiver"]:
         msg["To"] = receiver
         smtp.sendmail(config["sender"], receiver, msg.as_string())
@@ -48,19 +41,19 @@ def sendEmail(mail_content='测试', mail_title='辣鸡，又出错了...'):
 
 def notify(content, type=0):
     try:
-        if type==1:
+        if type == 1:
             sendEmail(content)
-        elif type==2:
+        elif type == 2:
             # requests.get('http://sc.ftqq.com/SCKEY.send')
             pass
         else:
             sendEmail(content)
             # requests.get('http://sc.ftqq.com/SCKEY.send')
     except Exception as e:
-        logger.info('notify error error %s', e)
+        logging.error('notify error error %s', e)
         pass
 
 
 if __name__ == '__main__':
-    logger.info(sendEmail('hello world.'))
+    logging.info(sendEmail('hello world.'))
     pass
